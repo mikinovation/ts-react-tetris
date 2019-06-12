e1032b0018768329ede0ce50b35fb2d07cc16670
mikinovation <maneuver2472@gmail.com>
2019-06-12 20:48:32 +0900

    Updates

---
+++ precache-manifest.4797187043185cf3cf19e83d944d08a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40f48805d2aff86d903d86fad5f4126e",
+    "url": "/ts-react-tetris/index.html"
+  },
+  {
+    "revision": "99628a8afb82c7f1c27f",
+    "url": "/ts-react-tetris/static/css/main.487fce45.chunk.css"
+  },
+  {
+    "revision": "43beb4689dff68018535",
+    "url": "/ts-react-tetris/static/js/2.e7dbb282.chunk.js"
+  },
+  {
+    "revision": "99628a8afb82c7f1c27f",
+    "url": "/ts-react-tetris/static/js/main.e4064138.chunk.js"
+  },
+  {
+    "revision": "7c79a156c5f4aff9ee67",
+    "url": "/ts-react-tetris/static/js/runtime~main.785ae077.js"
+  }
+]);
